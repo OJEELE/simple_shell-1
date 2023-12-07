7a92b752bb24e81b29202c426b7f0a2700787288
ojeele <ojeelesam@gmail.com>
2023-12-06 23:33:35 -0800

    custom strtoken function

--- _strtok.c
+++ _strtok.c
@@ -1,32 +1,32 @@
-#include <stdio,h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-/*_strtok - function that tokenizes string
- * @str: pointer to string to be seperated
- * @delim: delimeter to seperate the string
- * Return: returns character
- */
-void _strtok(const char *str, char *delim)
-{
-	int len = strlen(str);
-	char str[len + 1];
-	int i, j;
-	for (i = 0; i <= len; i++)
-	{
-		j = 0;
-		if (str[i] == delim || str[i] == '\0')
-			word[j] = '\0';
-		if (j > 0)
-		{
-			printf("%s\n", str);
-			j = 0;
-		}
-		else
-		{
-			word[j] = str[i];
-			j++;
-		}
 
-	}
-	return (0);
+/* _strtok - function that tokenizes string
+ * @str: pointer to string to be separated
+ * @delim: delimiter to separate the string
+ * Return: returns nothing (void)
+ */
+void _strtok(const char *str, char delim)
+{
+    int len = strlen(str);
+    char word[len + 1];
+    int j = 0;
+
+    for (int i = 0; i <= len; i++)
+    {
+        if (str[i] == delim || str[i] == '\0')
+        {
+            word[j] = '\0';
+            if (j > 0)
+            {
+                printf("%s\n", word);
+                j = 0;
+            }
+        }
+        else
+        {
+            word[j++] = str[i];
+        }
+    }
 }

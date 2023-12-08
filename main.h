c02ff97fbb63402341c1e766ece1486b42a01f54
ojeele <ojeelesam@gmail.com>
2023-12-08 01:36:23 -0800

    string functions

--- main.h
+++ main.h
@@ -9,10 +9,16 @@
 #include <stddef.h>
 
 void check_cmd(char *cmd);
-/* string tokenization  function */
+/*custom functions */
 
 void _strtok(const char *str, char delim);
 int _strlen(char *s);
+void _puts(char *str);
+char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+char *_strcpy(char *dest, char *src);
+int _strncmp(const char *s1, const char *s2, size_t n);
+int _putchar(char c);
 
 
 #endif/* MAIN_H */
